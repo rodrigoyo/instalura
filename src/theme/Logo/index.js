3ac12dba63f4e7dd1bfd82752833d693ea46b858
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2021-04-06 08:18:42 -0300

    fix: remove desnecessary log

--- src/theme/Logo/index.js
+++ src/theme/Logo/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export function Logo({ dark }) {
-    console.log(dark);
 
     const svgLogo = (dark) ?
         <svg width="96" height="24" viewBox="0 0 131 32" fill="none" xmlns="http://www.w3.org/2000/svg">
